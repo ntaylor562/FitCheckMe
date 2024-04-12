5069791443ce8b9f788160d8e635c512947521f2
Nathan Taylor <nathan.taylor562@gmail.com>
2024-04-11 20:03:09 -0700

    Initialized Vite React project for frontend

--- fitcheckme-frontend/src/components/OutfitTile.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-import { getAllOutfits } from "../services/OutfitService"
-
-export default function OutfitTile() {
-	getAllOutfits()
-	return <React.Fragment>Hello World</React.Fragment>
-}
--- fitcheckme-frontend/src/services/OutfitService.js
+++
@@ -1,7 +0,0 @@
-import config from "../config.json"
-
-export async function getAllOutfits() {
-	return await fetch(`${config.BACKEND_URL}/api/outfit`, {
-		method: "GET"
-	}).then((res) => res.json())
-}

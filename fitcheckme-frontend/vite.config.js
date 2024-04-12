b041f0bed3b6e4469d116ced618972682e19147b
ntaylor562 <nathan.taylor562@gmail.com>
2024-04-12 15:11:10 -0700

    Set up docker for frontend

--- fitcheckme-frontend/vite.config.js
+++ fitcheckme-frontend/vite.config.js
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	base: "/",
 	server: {
+		host: true,
+		origin: "http://localhost:3000",
 		port: 3000
 	}
 })

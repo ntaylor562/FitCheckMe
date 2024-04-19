ef47892cb19ad08e14c2131bec5dce4ded980745
Nathan Taylor <nathan.taylor562@gmail.com>
2024-04-19 14:13:16 -0700

    Added basic profile page component

--- fitcheckme-frontend/src/pages/Profile.jsx
+++ fitcheckme-frontend/src/pages/Profile.jsx
@@ -1,8 +1,12 @@
+import { Heading, Text, Image, Box } from '@chakra-ui/react'
+
 export default function Profile({ user }) {
 	return (
-		<div>
-			<h1>{user.username}'s Profile</h1>
-			<p>Bio: {user.bio}</p>
+		<div className="page profile-page">
+			<Image src='' borderRadius='full' boxSize='150px' alt='profile-picture' fallback={<div className='placeholder-image' style={{width: '150px', height: '150px', borderRadius: '50%'}}>placeholder</div>} />
+			<Heading paddingBottom='10px'>{user.username}'s Profile</Heading>
+			<Text paddingBottom='10px'>Bio: {user.bio}</Text>
+			<Box w='100%' border='solid 1px'>placeholder content</Box>
 		</div>
 	)
 }

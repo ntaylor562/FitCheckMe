f8860afc74739144128603d244f65566cb54022a
Nathan Taylor <nathan.taylor562@gmail.com>
2024-04-24 21:05:48 -0700

    Revert "Changed JwtAuthorizationFilter to a configuration as it contains a Bean"
    This reverts commit 2028bfde004bfb5e31c03dc14b1910f2f74961e5.

--- fitcheckme-backend/src/main/java/com/fitcheckme/FitCheckMe/auth/JwtAuthorizationFilter.java
+++ fitcheckme-backend/src/main/java/com/fitcheckme/FitCheckMe/auth/JwtAuthorizationFilter.java
@@ -6,12 +6,12 @@ import java.util.Map;
 
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -22,7 +22,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@Configuration
+@Component
 public class JwtAuthorizationFilter extends OncePerRequestFilter{
 	
 	private final JwtUtil jwtUtil;

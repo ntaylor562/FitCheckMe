b803c7fa67eea1808d2c8a9130d025527aa33d3a
ntaylor562 <nathan.taylor562@gmail.com>
2024-05-23 18:25:59 -0700

    Updated spring boot version and fixed contextLoads test

--- fitcheckme-backend/src/test/java/com/fitcheckme/FitCheckMe/FitCheckMeApplicationTests.java
+++ fitcheckme-backend/src/test/java/com/fitcheckme/FitCheckMe/FitCheckMeApplicationTests.java
@@ -2,9 +2,21 @@ package com.fitcheckme.FitCheckMe;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class FitCheckMeApplicationTests {
+	@Container
+	@ServiceConnection
+	protected static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15");
+
+	static {
+		postgres.start();
+	}
 
 	@Test
 	void contextLoads() {

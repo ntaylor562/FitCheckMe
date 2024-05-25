6f93ce6c0bbb12312acc5a938d95b39c4528c4b2
Nathan Taylor <nathan.taylor562@gmail.com>
2024-05-25 15:30:15 -0700

    Created a basic user settings page. Need to connect some settings to backend

--- fitcheckme-frontend/src/pages/UserSettings/AccountSettings.jsx
+++ fitcheckme-frontend/src/pages/UserSettings/AccountSettings.jsx
@@ -1,16 +1,124 @@
-import { Box, FormControl, FormLabel, Heading, Input } from "@chakra-ui/react";
+import { Box, Button, Divider, Flex, FormControl, FormLabel, Grid, GridItem, HStack, Heading, Input, InputGroup, InputRightAddon, Modal, ModalBody, ModalContent, ModalHeader, ModalOverlay, SimpleGrid, Spacer, StackDivider, Text, VStack, useDisclosure, useToast } from "@chakra-ui/react";
+import { useAuth } from "../../contexts/AuthContext";
+import { useEffect, useState } from "react";
+import { editUser } from "../../backend/Application";
 
 
 export default function AccountSettings() {
+	const { currentUser, isLoading, setCurrentUser } = useAuth();
+	const [formUsername, setFormUsername] = useState("");
+	const { isOpen, onOpen, onClose } = useDisclosure();
+	const toast = useToast();
+
+	useEffect(() => {
+		if (!isLoading && currentUser)
+			setFormUsername(currentUser.username);
+	}, [isLoading]);
+
+	const onUsernameChange = (e) => {
+		setFormUsername(e.target.value);
+	}
+
+	const onUsernameSubmit = (e) => {
+		e.preventDefault();
+		onOpen();
+	}
+
+	const handleUpdateUsername = async () => {
+		editUser(currentUser.userId, formUsername, null)
+			.then(async (response) => {
+				if (!response.ok) {
+					const contentType = response.headers.get("content-type");
+					const message = contentType && contentType.includes("application/json") ? (await response.json()).message : await response.text();
+					toast({
+						title: 'Error updating username.',
+						description: message,
+						status: 'error',
+						duration: 5000,
+						isClosable: true,
+					});
+				}
+				else {
+					onClose();
+					toast({
+						title: 'Username updated.',
+						description: `Your username has been updated to ${formUsername}.`,
+						status: 'success',
+						duration: 5000,
+						isClosable: true,
+					});
+					const newCurrentUser = { ...currentUser, username: formUsername };
+					setCurrentUser(newCurrentUser);
+				}
+			});
+	}
+
 	return (
 		<Box className="subpage">
-			<Heading as="h1" size="lg">Account Settings</Heading>
-			<form>
-				<FormControl>
-					<FormLabel>Username</FormLabel>
-					<Input type="text" />
-				</FormControl>
-			</form>
+			<Heading as="h1" size="xl">Account Settings</Heading>
+			<br />
+			<Divider />
+			<br />
+			<VStack divider={<StackDivider />} alignItems="baseline" spacing={10}>
+				<form onSubmit={onUsernameSubmit}>
+					<FormControl>
+						<Grid templateColumns="1fr 3fr">
+							<GridItem alignContent="center">
+								<FormControl w="fit-content">Username:</FormControl>
+							</GridItem>
+							<GridItem>
+								<InputGroup>
+									<Input value={formUsername} name="username" w="20em" type="text" onChange={onUsernameChange} />
+									<InputRightAddon p="0px">
+										<Button borderLeftRadius="unset" type="submit" w="100%">Save</Button>
+									</InputRightAddon>
+								</InputGroup>
+							</GridItem>
+						</Grid>
+						<Modal isOpen={isOpen} onClose={onClose}>
+							<ModalOverlay />
+							<ModalContent>
+								<ModalHeader>Are you sure you want to change your username to {formUsername}?</ModalHeader>
+								<ModalBody>
+									<SimpleGrid columns={2} spacing={10}>
+										<Button colorScheme="red" onClick={() => {
+											setFormUsername(currentUser.username);
+											onClose();
+										}}>Cancel</Button>
+										<Button colorScheme="green" onClick={handleUpdateUsername}>Confirm</Button>
+									</SimpleGrid>
+								</ModalBody>
+							</ModalContent>
+						</Modal>
+					</FormControl>
+				</form>
+
+				<form>
+					<FormControl>
+						<FormLabel><Heading as="h2" size="lg">Update Password</Heading></FormLabel>
+						<Grid templateColumns="1fr 3fr" gap={6}>
+							<GridItem alignContent="center">
+								<FormControl w="fit-content">Old password:</FormControl>
+							</GridItem>
+							<GridItem alignContent="center">
+								<Input name="oldPassword" w="20em" type="password" />
+							</GridItem>
+							<GridItem alignContent="center">
+								<FormControl w="fit-content">New password:</FormControl>
+							</GridItem>
+							<GridItem alignContent="center">
+								<Input name="newPassword" w="20em" type="password" />
+							</GridItem>
+							<GridItem alignContent="center">
+								<FormControl w="fit-content">Confirm password:</FormControl>
+							</GridItem>
+							<GridItem alignContent="center">
+								<Input name="newPasswordConfirm" w="20em" type="password" />
+							</GridItem>
+						</Grid>
+					</FormControl>
+				</form>
+			</VStack>
 		</Box>
 	)
 }
--- fitcheckme-frontend/src/pages/UserSettings/AppSettings.jsx
+++ fitcheckme-frontend/src/pages/UserSettings/AppSettings.jsx
@@ -4,7 +4,7 @@ import { Box, Heading } from "@chakra-ui/react";
 export default function AppSettings() {
 	return (
 		<Box className="subpage">
-			<Heading as="h1" size="lg">App Settings</Heading>
+			<Heading as="h1" size="xl">App Settings</Heading>
 		</Box>
 	)
 }

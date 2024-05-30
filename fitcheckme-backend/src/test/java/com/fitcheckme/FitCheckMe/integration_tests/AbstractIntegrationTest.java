9bc88691abf5ca36ea1746ff961b51ebe1eda0a9
ntaylor562 <nathan.taylor562@gmail.com>
2024-05-30 14:12:11 -0700

    Fixed consecutive integration tests failing and created new helper methods for integration tests

--- fitcheckme-backend/src/test/java/com/fitcheckme/FitCheckMe/integration_tests/AbstractIntegrationTest.java
+++ fitcheckme-backend/src/test/java/com/fitcheckme/FitCheckMe/integration_tests/AbstractIntegrationTest.java
@@ -1,9 +1,12 @@
 package com.fitcheckme.FitCheckMe.integration_tests;
 
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -11,15 +14,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.access.AccessDeniedException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.util.FileCopyUtils;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fitcheckme.FitCheckMe.DTOs.auth.UserLoginRequestDTO;
 import com.fitcheckme.FitCheckMe.DTOs.auth.UserLoginReturnDTO;
@@ -28,22 +34,21 @@ import com.fitcheckme.FitCheckMe.DTOs.auth.UserLoginReturnDTO;
 @EnableConfigurationProperties
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
-@Testcontainers
 @ActiveProfiles("test")
 public abstract class AbstractIntegrationTest {
-	@Container
 	@ServiceConnection
 	protected static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15");
 
 	@Autowired
 	protected TestRestTemplate restTemplate;
 
+	@Autowired
+	protected JdbcTemplate jdbcTemplate;
+
 	protected static final ObjectMapper mapper = new ObjectMapper();
 
-	protected static <T> ParameterizedTypeReference<List<T>> getTypeOfListOfType(Class<T> classType) {
-		return new ParameterizedTypeReference<List<T>>() {
-		};
-	}
+	private static String accessTokenCookie;
+	private static String refreshTokenCookie;
 
 	static {
 		postgres.start();
@@ -51,24 +56,74 @@ public abstract class AbstractIntegrationTest {
 
 	@BeforeAll
 	protected void setup() {
-		// Logging in as a test user and setting cookies for the rest template
-		//TODO get the tokens from the cookies instead of the response body in case they're removed from response body
-		UserLoginReturnDTO result = Optional.of(
-				restTemplate.exchange(
-						"/api/auth/login",
-						HttpMethod.POST,
-						new HttpEntity<>(new UserLoginRequestDTO("test", "test")),
-						UserLoginReturnDTO.class)
-						.getBody())
-				.orElseThrow(() -> new AccessDeniedException("Unable to log in for integration tests"));
+		postgres.start();
 
-		restTemplate.getRestTemplate().getInterceptors().add(((request, body, execution) -> {
-			request.getHeaders().add("Cookie",
-					"jwt-access-token=" + result.accessToken() + "; Path=/; Secure; HttpOnly;");
-			request.getHeaders().add("Cookie",
-					"jwt-refresh-token=" + result.refreshToken() + "; Path=/api/auth; Secure; HttpOnly;");
+		Resource resource = new ClassPathResource("data-test.sql");
+		try {
+			// Populate DB with sample data in data sql file
+			jdbcTemplate.execute(FileCopyUtils
+					.copyToString(new InputStreamReader(resource.getInputStream(), StandardCharsets.UTF_8)));
+		} catch (Exception e) {
+			throw new RuntimeException(String.format("ERROR reading file for inital test data: %s", e.toString()));
+		}
+
+		// Logging in as a test user and setting cookies for the rest template
+		// TODO get the tokens from the cookies instead of the response body in case
+		// they're removed from response body
+		UserLoginReturnDTO result = getObjectFromResponse(
+				postCall("/api/auth/login",
+						new UserLoginRequestDTO("test", "test")),
+				UserLoginReturnDTO.class);
+
+		AbstractIntegrationTest.accessTokenCookie = "jwt-access-token=" + result.accessToken()
+				+ "; Path=/; Secure; HttpOnly;";
+		AbstractIntegrationTest.refreshTokenCookie = "jwt-refresh-token=" + result.refreshToken()
+				+ "; Path=/api/auth; Secure; HttpOnly;";
+	}
+
+	@BeforeEach
+	protected void addAuthTokensToRestTemplate() {
+		restTemplate.getRestTemplate().getInterceptors().add((request, body, execution) -> {
+			request.getHeaders().add("Cookie", AbstractIntegrationTest.accessTokenCookie);
+			request.getHeaders().add("Cookie", AbstractIntegrationTest.refreshTokenCookie);
 			return execution.execute(request, body);
-		}));
+		});
+	}
+
+	protected static <T> ParameterizedTypeReference<List<T>> getTypeOfListOfType(Class<T> classType) {
+		return new ParameterizedTypeReference<List<T>>() {
+		};
+	}
+
+	public <T> ResponseEntity<T> restCall(String url, HttpMethod method, HttpEntity<T> requestEntity) {
+		ResponseEntity<T> res = restTemplate.exchange(url, method, requestEntity, new ParameterizedTypeReference<T>() {
+		});
+		if (res.getStatusCode().isError()) {
+			throw new RuntimeException("Error in rest call: " + res.toString());
+		}
+
+		return res;
+	}
+
+	public ResponseEntity<Object> restCall(String url, HttpMethod method) {
+		return restCall(url, method, null);
+	}
+
+	public ResponseEntity<Object> getCall(String url) {
+		return restCall(url, HttpMethod.GET);
+	}
+
+	public <T> ResponseEntity<T> postCall(String url, T body) {
+		return restCall(url, HttpMethod.POST, new HttpEntity<T>(body));
+	}
+
+	public <T> T getObjectFromResponse(ResponseEntity<Object> response, Class<T> classType) {
+		return mapper.convertValue(Optional.of(response.getBody()).get(), classType);
+	}
+
+	public <T> List<T> getListOfObjectsFromResponse(ResponseEntity<Object> response, Class<T> classType) {
+		return mapper.convertValue(Optional.of(response.getBody()).get(), new TypeReference<List<T>>() {
+		}).stream().map(tag -> mapper.convertValue(tag, classType)).toList();
 	}
 
 }
--- fitcheckme-backend/src/test/java/com/fitcheckme/FitCheckMe/integration_tests/TagControllerIntegrationTest.java
+++ fitcheckme-backend/src/test/java/com/fitcheckme/FitCheckMe/integration_tests/TagControllerIntegrationTest.java
@@ -3,11 +3,10 @@ package com.fitcheckme.FitCheckMe.integration_tests;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
-import java.util.Optional;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.Rollback;
 
@@ -19,14 +18,27 @@ public class TagControllerIntegrationTest extends AbstractIntegrationTest {
 	@Autowired
 	private TagRepository tagRepository;
 
+	@BeforeEach
+	public void wipeDatabase() {
+		tagRepository.deleteAll();
+	}
+
+	@Test
+	@Rollback
+	public void testGetAllTagsAndExpectEmptyList() {
+		ResponseEntity<Object> response = getCall("/api/tag/all");
+		List<TagRequestDTO> tags = getListOfObjectsFromResponse(response, TagRequestDTO.class);
+		assertThat(tags).isEmpty();
+	}
+
 	@Test
 	@Rollback
 	public void testGetAllTags() {
 		Tag tag1 = tagRepository.save(new Tag("tag1"));
 		Tag tag2 = tagRepository.save(new Tag("tag2"));
-		
-		ResponseEntity<List<Object>> response = restTemplate.exchange("/api/tag/all", HttpMethod.GET, null, getTypeOfListOfType(Object.class));
-		List<TagRequestDTO> tags = Optional.of(response.getBody()).get().stream().map(tag -> mapper.convertValue(tag, TagRequestDTO.class)).toList();
+
+		ResponseEntity<Object> response = getCall("/api/tag/all");
+		List<TagRequestDTO> tags = getListOfObjectsFromResponse(response, TagRequestDTO.class);
 		assertThat(tags).containsExactly(TagRequestDTO.toDTO(tag1), TagRequestDTO.toDTO(tag2));
 	}
 }

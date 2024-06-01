44f9ae085d9802d6246121247e5a07551fa35a7c
Nathan Taylor <nathan.taylor562@gmail.com>
2024-06-01 09:51:30 -0700

    Finished user controller integration tests

--- fitcheckme-backend/src/test/java/com/fitcheckme/FitCheckMe/integration_tests/UserControllerIntegrationTest.java
+++ fitcheckme-backend/src/test/java/com/fitcheckme/FitCheckMe/integration_tests/UserControllerIntegrationTest.java
@@ -2,6 +2,7 @@ package com.fitcheckme.FitCheckMe.integration_tests;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -15,8 +16,14 @@ import com.fitcheckme.FitCheckMe.DTOs.User.UserRequestDTO;
 import com.fitcheckme.FitCheckMe.DTOs.User.UserRoleUpdateDTO;
 import com.fitcheckme.FitCheckMe.DTOs.User.UserUpdateDetailsRequestDTO;
 import com.fitcheckme.FitCheckMe.DTOs.User.UserUpdatePasswordRequestDTO;
+import com.fitcheckme.FitCheckMe.models.Garment;
+import com.fitcheckme.FitCheckMe.models.Outfit;
 import com.fitcheckme.FitCheckMe.models.Role;
+import com.fitcheckme.FitCheckMe.models.Tag;
 import com.fitcheckme.FitCheckMe.models.User;
+import com.fitcheckme.FitCheckMe.repositories.GarmentRepository;
+import com.fitcheckme.FitCheckMe.repositories.OutfitRepository;
+import com.fitcheckme.FitCheckMe.repositories.TagRepository;
 import com.fitcheckme.FitCheckMe.repositories.UserRepository;
 
 public class UserControllerIntegrationTest extends AbstractIntegrationTest {
@@ -24,9 +31,24 @@ public class UserControllerIntegrationTest extends AbstractIntegrationTest {
 	@Autowired
 	private UserRepository userRepository;
 
+	@Autowired
+	private GarmentRepository garmentRepository;
+
+	@Autowired
+	private OutfitRepository outfitRepository;
+
+	@Autowired
+	private TagRepository tagRepository;
+
 	@BeforeEach
 	public void setup() {
 		resetAuth();
+	}
+
+	private User createTestUser() {
+		ResponseEntity<Object> createUserResponse = postCall("/api/user/create",
+				new UserCreateRequestDTO("new_test_user", "new_test_user@test.com", "password"));
+		return userRepository.findById(getObjectFromResponse(createUserResponse, UserRequestDTO.class).userId()).get();
 	}
 
 	@Test
@@ -188,21 +210,19 @@ public class UserControllerIntegrationTest extends AbstractIntegrationTest {
 
 	@Test
 	public void testUpdateUserDetailsAndExpectUser() {
-		ResponseEntity<Object> createUserResponse = postCall("/api/user/create",
-				new UserCreateRequestDTO("new_test_user", "new_test_user@test.com", "password"));
-		UserRequestDTO createdUser = getObjectFromResponse(createUserResponse, UserRequestDTO.class);
+		User createdUser = createTestUser();
 
 		logout();
 		login("new_test_user", "password");
 
 		ResponseEntity<Object> updateUserResponse = putCall("/api/user/details",
-				new UserUpdateDetailsRequestDTO(createdUser.userId(), "new_test_user_updated", "new bio"));
+				new UserUpdateDetailsRequestDTO(createdUser.getId(), "new_test_user_updated", "new bio"));
 		UserRequestDTO updatedUser = getObjectFromResponse(updateUserResponse, UserRequestDTO.class);
 
-		User updatedUserEntity = userRepository.findById(createdUser.userId()).get();
+		User updatedUserEntity = userRepository.findById(createdUser.getId()).get();
 
 		assertThat(updateUserResponse.getStatusCode().isError()).isFalse();
-		assertThat(updatedUser.userId()).isEqualTo(createdUser.userId());
+		assertThat(updatedUser.userId()).isEqualTo(createdUser.getId());
 		assertThat(updatedUser.username()).isEqualTo("new_test_user_updated")
 				.isEqualTo(updatedUserEntity.getUsername());
 		assertThat(updatedUser.bio()).isEqualTo("new bio").isEqualTo(updatedUserEntity.getBio());
@@ -213,20 +233,18 @@ public class UserControllerIntegrationTest extends AbstractIntegrationTest {
 
 	@Test
 	public void testUpdateUserDetailsWithExistingUsernameAndExpectBadRequest() {
-		ResponseEntity<Object> createUserResponse = postCall("/api/user/create",
-				new UserCreateRequestDTO("new_test_user", "new_test_user@test.com", "password"));
-		UserRequestDTO createdUser = getObjectFromResponse(createUserResponse, UserRequestDTO.class);
+		User createdUser = createTestUser();
 
 		logout();
 		login("new_test_user", "password");
 
 		ResponseEntity<Object> updateUserResponse = putCall("/api/user/details",
-				new UserUpdateDetailsRequestDTO(createdUser.userId(), "test_user", "new bio"), true);
+				new UserUpdateDetailsRequestDTO(createdUser.getId(), "test_user", "new bio"), true);
 
-		User newUserEntity = userRepository.findById(createdUser.userId()).get();
+		User newUserEntity = userRepository.findById(createdUser.getId()).get();
 
 		assertThat(updateUserResponse.getStatusCode()).isEqualTo(HttpStatus.CONFLICT);
-		assertThat(newUserEntity.getId()).isEqualTo(createdUser.userId());
+		assertThat(newUserEntity.getId()).isEqualTo(createdUser.getId());
 		assertThat(newUserEntity.getUsername()).isEqualTo("new_test_user");
 		assertThat(newUserEntity.getBio()).isEqualTo(null);
 
@@ -239,28 +257,26 @@ public class UserControllerIntegrationTest extends AbstractIntegrationTest {
 		logout();
 		login("test_super_admin");
 
-		ResponseEntity<Object> createUserResponse = postCall("/api/user/create",
-				new UserCreateRequestDTO("new_test_user", "new_test_user@test.com", "password"));
-		UserRequestDTO createdUser = getObjectFromResponse(createUserResponse, UserRequestDTO.class);
+		User createdUser = createTestUser();
 
 		ResponseEntity<Object> addRoleResponse = putCall("/api/user/addrole",
-				new UserRoleUpdateDTO(createdUser.userId(), "SUPER_ADMIN"));
+				new UserRoleUpdateDTO(createdUser.getId(), "SUPER_ADMIN"));
 
-		List<Role> roles = userRepository.findRolesByUserId(createdUser.userId());
+		List<Role> roles = userRepository.findRolesByUserId(createdUser.getId());
 
 		assertThat(addRoleResponse.getStatusCode()).isEqualTo(HttpStatus.ACCEPTED);
 		assertThat(roles.stream().anyMatch(r -> r.getName().equals("SUPER_ADMIN"))).isTrue();
 
 		ResponseEntity<Object> removeRoleResponse = putCall("/api/user/removerole",
-				new UserRoleUpdateDTO(createdUser.userId(), "SUPER_ADMIN"));
+				new UserRoleUpdateDTO(createdUser.getId(), "SUPER_ADMIN"));
 
-		roles = userRepository.findRolesByUserId(createdUser.userId());
+		roles = userRepository.findRolesByUserId(createdUser.getId());
 
 		assertThat(removeRoleResponse.getStatusCode()).isEqualTo(HttpStatus.ACCEPTED);
 		assertThat(roles.stream().anyMatch(r -> r.getName().equals("SUPER_ADMIN"))).isFalse();
 
 		logout();
-		userRepository.deleteById(createdUser.userId());
+		userRepository.deleteById(createdUser.getId());
 	}
 
 	@Test
@@ -281,19 +297,17 @@ public class UserControllerIntegrationTest extends AbstractIntegrationTest {
 
 	@Test
 	public void testUpdateUserPasswordAndExpectAccepted() {
-		ResponseEntity<Object> createUserResponse = postCall("/api/user/create",
-				new UserCreateRequestDTO("new_test_user", "new_test_user@test.com", "password"));
-		UserRequestDTO createdUser = getObjectFromResponse(createUserResponse, UserRequestDTO.class);
+		User createdUser = createTestUser();
 
 		logout();
 		login("new_test_user", "password");
 
-		String oldPassHash = userRepository.findById(createdUser.userId()).get().getPassword();
+		String oldPassHash = userRepository.findById(createdUser.getId()).get().getPassword();
 
 		ResponseEntity<Object> updatePasswordResponse = putCall("/api/user/password",
-				new UserUpdatePasswordRequestDTO(createdUser.userId(), "password", "password2"));
+				new UserUpdatePasswordRequestDTO(createdUser.getId(), "password", "password2"));
 
-		String newPassHash = userRepository.findById(createdUser.userId()).get().getPassword();
+		String newPassHash = userRepository.findById(createdUser.getId()).get().getPassword();
 
 		logout();
 		login("new_test_user", "password2");
@@ -307,19 +321,17 @@ public class UserControllerIntegrationTest extends AbstractIntegrationTest {
 
 	@Test
 	public void testUpdateUserPasswordWithBadOldPasswordAndExpectBadRequest() {
-		ResponseEntity<Object> createUserResponse = postCall("/api/user/create",
-				new UserCreateRequestDTO("new_test_user", "new_test_user@test.com", "password"));
-		UserRequestDTO createdUser = getObjectFromResponse(createUserResponse, UserRequestDTO.class);
+		User createdUser = createTestUser();
 
 		logout();
 		login("new_test_user", "password");
 
-		String oldPassHash = userRepository.findById(createdUser.userId()).get().getPassword();
+		String oldPassHash = userRepository.findById(createdUser.getId()).get().getPassword();
 
 		ResponseEntity<Object> updatePasswordResponse = putCall("/api/user/password",
-				new UserUpdatePasswordRequestDTO(createdUser.userId(), "wrong_password", "password2"), true);
+				new UserUpdatePasswordRequestDTO(createdUser.getId(), "wrong_password", "password2"), true);
 
-		String newPassHash = userRepository.findById(createdUser.userId()).get().getPassword();
+		String newPassHash = userRepository.findById(createdUser.getId()).get().getPassword();
 
 		logout();
 		login("new_test_user", "password");
@@ -347,7 +359,42 @@ public class UserControllerIntegrationTest extends AbstractIntegrationTest {
 
 	@Test
 	public void testDeleteUserAndExpectUserDeleted() {
-		//TODO
+		User createdUser = createTestUser();
+		
+		logout();
+		login("new_test_user", "password");
+
+		Tag tag1 = tagRepository.save(new Tag("tag 1"));
+		Garment garment1 = garmentRepository.save(new Garment(createdUser, "garment 1", List.of("url1", "url2"), List.of(tag1)));
+		Outfit outfit1 = outfitRepository.save(new Outfit(createdUser, "outfit 1", "outfit desc", LocalDateTime.now(), List.of(garment1), List.of(tag1)));
+
+		ResponseEntity<Object> deleteUserResponse = deleteCall(String.format("/api/user?id=%d", createdUser.getId()));
+
+		assertThat(deleteUserResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(userRepository.findById(createdUser.getId()).isEmpty()).isTrue();
+		assertThat(garmentRepository.findById(garment1.getId()).isEmpty()).isTrue();
+		assertThat(outfitRepository.findById(outfit1.getId()).isEmpty()).isTrue();
+		assertThat(tagRepository.findById(tag1.getId()).isEmpty()).isFalse();
+
+		tagRepository.delete(tag1);
+	}
+
+	@Test
+	public void testDeleteUserAndExpectUserNotFound() {
+		ResponseEntity<Object> deleteUserResponse = deleteCall("/api/user?id=999", true);
+
+		assertThat(deleteUserResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+	}
+
+	@Test
+	public void testDeleteUserAsDifferentUserAndExpectForbidden() {
+		User testUser = createTestUser();
+		ResponseEntity<Object> deleteUserResponse = deleteCall(String.format("/api/user?id=%d", testUser.getId()), true);
+
+		assertThat(deleteUserResponse.getStatusCode()).isEqualTo(HttpStatus.FORBIDDEN);
+		assertThat(userRepository.findById(testUser.getId()).isEmpty()).isFalse();
+
+		userRepository.delete(testUser);
 	}
 
 }
